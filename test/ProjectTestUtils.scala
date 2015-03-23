0429234ae7c915be2cfd843315147a6273455659
liviu.ignat <liviu.ignat@check24.de>
2015-03-23 19:07:31 +0100

    Add more tests for create user and get user

--- test/ProjectTestUtils.scala
+++ test/ProjectTestUtils.scala
@@ -4,7 +4,8 @@ import play.api.Application
 import play.api.test.FakeApplication
 import play.api.test.Helpers._
 import play.modules.reactivemongo.ReactiveMongoPlugin
-import reactivemongo.api.{MongoConnection, MongoDriver}
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
 
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{Await, Future}
@@ -15,17 +16,18 @@ import play.api.libs.concurrent.Execution.Implicits._
  * Created by liviuignat on 21/03/15.
  */
 object ProjectTestUtils {
-  def runWithTestDatabase[T](block: => T) {
-    //val fakeApp = FakeApplication(additionalConfiguration = inMemoryDatabase())
-    val fakeApplication = FakeApplication();
+  def fakeApplication (): Application = {
+    FakeApplication(additionalConfiguration = inMemoryDatabase())
+    FakeApplication()
+  }
 
+  def runWithTestDatabase[T](block: => T) {
     running(fakeApplication) {
-      //ProjectRepositoryFake.insertTestDataIfEmpty()
       block
     }
   }
 
-  def clearDbData(): Boolean = {
+  def dbConnection: DefaultDB = {
     val uri = "mongodb://scalaplaymongo:scalaplaymongo123@ds045521.mongolab.com:45521/heroku_app35066918"
     val driver = new MongoDriver
     val connection: Try[MongoConnection] =
@@ -33,12 +35,14 @@ object ProjectTestUtils {
         driver.connection(parsedUri)
       }
 
-    val db = connection.get.db("heroku_app35066918")
+    connection.get.db("heroku_app35066918")
+  }
 
+  def dropDb(): Boolean = {
+    val db = dbConnection;
     val result = Await.result(db.drop().map {
       success => success
     }, FiniteDuration(5, TimeUnit.SECONDS))
-
     result
   }
 }
--- test/controllers/UsersControllerSpec.scala
+++ test/controllers/UsersControllerSpec.scala
@@ -13,91 +13,101 @@ import scala.concurrent.duration.FiniteDuration
 
 class UsersControllerSpec extends JasmineSpec with BeforeAndAfter with BeforeAndAfterAll with ShouldMatchers {
   val timeout: FiniteDuration = FiniteDuration(5, TimeUnit.SECONDS)
-  val fakeApplication = FakeApplication();
   var firstUserId : String = "";
 
-  Play.start(fakeApplication)
-  ProjectTestUtils.clearDbData
+  override def beforeAll() = {
+    Play.start(ProjectTestUtils.fakeApplication)
+  }
 
   override def afterAll() = {
     Play.stop
   }
 
-  describe("When wanting to insert a first user") {
-    var response: Option[Future[Result]] = null
+  describe("UsersControllerSpec") {
     beforeEach {
-      val request = FakeRequest.apply("POST", "/user")
-        .withJsonBody(Json.obj(
-        "email" -> "liviu@ignat.email",
-        "password" -> "test123",
-        "firstName" -> "Liviu",
-        "lastName" -> "Ignat"))
-      response = route(request)
-      val result = Await.result(response.get, timeout)
+      ProjectTestUtils.dropDb()
     }
 
-    it("Should be able to the request with success") {
-      response.isDefined should equal(true)
-    }
-
-    it("Should have the Status Header Created") {
-      val result = Await.result(response.get, timeout)
-      result.header.status should equal(CREATED)
-    }
-
-    it("Should have the Location header defined") {
-      val result = Await.result(response.get, timeout)
-      assert(result.header.headers.contains("Location") == true)
-      firstUserId = result.header.headers.get("Location").get
-    }
-
-    it("Should have the Location header with length 24") {
-      val result = Await.result(response.get, timeout)
-      result.header.headers.get("Location").get.length should equal(24)
-    }
-
-    describe("When getting first user") {
+    describe("When wanting to insert a first user") {
       var response: Option[Future[Result]] = null
-      beforeEach {
-        val uri = s"/user/$firstUserId"
-        println(uri)
-        val request = FakeRequest.apply("PUT", uri)
-        response = route(request)
-      }
-
-      it("Should be able make the request with success") {
-        response.isDefined should equal(true)
-        val result = Await.result(response.get, timeout)
-        result.header.status should equal(OK)
-      }
-
-      it("Json response should not contain password") {
-        val json: JsValue = contentAsJson(response.get)
-        val password = json.\("password").toString()
-        println(password)
-      }
-    }
-
-    describe("When wanting to insert a second user") {
-      var response: Option[Future[Result]] = null
+      var result: Result = null
       beforeEach {
         val request = FakeRequest.apply("POST", "/user")
           .withJsonBody(Json.obj(
-          "email" -> "liviu.ignat@someotheremail.com",
-          "password" -> "someotheremailtest123",
-          "firstName" -> "Liviu Marius",
-          "lastName" -> "Ignat Someother"))
+          "email" -> "liviu@ignat.email",
+          "password" -> "test123",
+          "firstName" -> "Liviu",
+          "lastName" -> "Ignat"))
         response = route(request)
-        val result = Await.result(response.get, timeout)
+
+        result = Await.result(response.get, timeout)
+        firstUserId = result.header.headers.get("Location").get
       }
 
-      it("Should be able make the request with success") {
+      it("Should be able to the request with success") {
         response.isDefined should equal(true)
+      }
 
-        val result = Await.result(response.get, timeout)
+      it("Should have the Status Header Created") {
         result.header.status should equal(CREATED)
+      }
+
+      it("Should have the Location header defined") {
+        assert(result.header.headers.contains("Location") == true)
+      }
+
+      it("Should have the Location header with length 24") {
+        result.header.headers.get("Location").get.length should equal(24)
+      }
+
+      describe("When getting first user") {
+        var response: Option[Future[Result]] = null
+        var result: Result = null
+        beforeEach {
+          val uri = s"/user/$firstUserId"
+          val request = FakeRequest.apply("PUT", uri)
+          response = route(request)
+          result = Await.result(response.get, timeout)
+        }
+
+        it("Should have a defined response") {
+          response.isDefined should equal(true)
+        }
+
+        it("Should be able make the request with success") {
+          result.header.status should equal(OK)
+        }
+
+        it("Json response should not contain password") {
+          val json: JsValue = contentAsJson(response.get)
+          val password = json.\("password")
+
+          assert(password.getClass == (new JsUndefined("")).getClass)
+        }
+      }
+
+      describe("When wanting to insert a second user") {
+        var response: Option[Future[Result]] = null
+        beforeEach {
+          val request = FakeRequest.apply("POST", "/user")
+            .withJsonBody(Json.obj(
+            "email" -> "liviu.ignat@someotheremail.com",
+            "password" -> "someotheremailtest123",
+            "firstName" -> "Liviu Marius",
+            "lastName" -> "Ignat Someother"))
+          response = route(request)
+          val result = Await.result(response.get, timeout)
+        }
+
+        it("Should be able make the request with success") {
+          response.isDefined should equal(true)
+
+          val result = Await.result(response.get, timeout)
+          result.header.status should equal(CREATED)
+        }
       }
     }
   }
+
 
 }
